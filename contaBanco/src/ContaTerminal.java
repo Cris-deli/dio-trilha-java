143b0ceba2bf044d2011c5a16f5c574c8d42b70f
Cris-deli <ccsacris78@gmail.com>
2024-05-26 16:56:34 -0300

    Desafio Conta banco

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -7,23 +7,20 @@ public class ContaTerminal {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Porfavor, digite o número da Agência: ");
-        String agencia = scanner.nextLine();
+            String agencia = scanner.nextLine();
     
         System.out.print("Por favor, digite o número da Conta: ");
-        int numero = scanner.nextInt();
-        scanner.nextLine(); // Limpar o buffer do scanner
+            int numero = scanner.nextInt();
+                scanner.nextLine(); // Limpar o buffer do scanner
     
         System.out.print("Por favor, digite o nome do Cliente: ");
-        String nomeCliente = scanner.nextLine();
+            String nomeCliente = scanner.nextLine();
     
         System.out.print("Por favor, digite o saldo da conta: ");
-        double saldo = scanner.nextDouble();
+            double saldo = scanner.nextDouble();
     
-        // Exibir mensagem final com os dados inseridos pelo usuário
-        System.out.println("Olá " + nomeCliente + 
-        ", obrigado por criar uma conta em nosso banco, sua agência é " 
-        + agencia + ", conta " + numero + " e seu saldo " + saldo + 
-        " já está disponível para saque.");
+        //TODO:exibir a mensagem final
+         System.out.println("Olá " + nomeCliente + "obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
         
         // Fechar o scanner
         scanner.close();
